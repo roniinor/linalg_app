8eb222f5c07235a55705e3bad33f08d23ecab7c3
roni <roni.malek@gmail.com>
2017-05-03 20:58:27 +0100

    python function return - simple single value passed through view function to template to display on page

--- linalg/views.py
+++ linalg/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def linalg_menu(request):
-    return render(request, 'linalg/menu.html', {})
+    return render(request, 'linalg/menu.html', {'num': get_a_number()})
+    
+    
+def get_a_number():
+    return 14;
