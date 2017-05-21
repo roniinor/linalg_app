71711dcbd7761b7d476bd905ab376b9b470bd8a5
roni <roni.malek@gmail.com>
2017-05-21 17:19:53 +0100

    refactored base templates

--- linalg/views.py
+++ linalg/views.py
@@ -5,10 +5,10 @@ def linalg_menu(request):
     return render(request, 'linalg/menu.html', {'num': get_a_number()})
     
 def linalg_asciiArt(request):
-    return render(request, 'linalg/ascArt.html', {'art': improc4.strPic()})
+    return render(request, 'linalg/ascArt.html', {'art': improc4.strPic(), 'title': "Ascii Art"})
     
 def linalg_matPage(request):
-    return render(request, 'linalg/matPage.html', {'mat': improc4.matrixFunction()})
+    return render(request, 'linalg/matPage.html', {'mat': improc4.matrixFunction(), 'title': "Other Mat Fn"})
 
 def get_a_number():
     return 14;
